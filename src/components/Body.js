c8d4ee9f7f5a7d8f9a257205be42953082ab1bec
bhutkarsayali <bhutkarsayali@gmail.com>
2023-12-18 12:27:09 +0530

    Commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -66,9 +66,8 @@ const Body = () => {
           type="submit"
           className="flex-none rounded-md bg-blue-900 px-3.5 py-2.5 text-sm font-semibold text-white shadow-lg hover:bg-blue-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
           onClick={() => {
-            const text = searchText.toLowerCase();
             let filterdRestaurents = listOfRestaurents.filter((res) =>
-            res.info.name.toLowerCase().includes(text)
+              res.info.name.toLowerCase().includes(searchText.toLowerCase())
             );
             console.log(filterdRestaurents);
             setListOfRestaurents(filterdRestaurents);
